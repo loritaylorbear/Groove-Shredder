b0e1c575149bae19691c3956c7bb3f9439a36821
rgarg8 <cryptolemy@gmail.com>
2011-05-24 17:20:51 -0400

    Eradicated namespace polution in overlay.js, updated version to 1.0

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -1,19 +1,21 @@
-var pref_service = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
-var gpreferences = pref_service.getBranch("extensions.grooveshredder");
+var orgArgeeCodeGrooveShredder = {};
 
-var grooveshredder = {
+orgArgeeCodeGrooveShredder.pref_service = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
+orgArgeeCodeGrooveShredder.gpreferences = orgArgeeCodeGrooveShredder.pref_service.getBranch("extensions.grooveshredder");
+
+orgArgeeCodeGrooveShredder.grooveshredder = {
   onLoad: function() {
     // initialization code
-    grooveRequestObserver.register();
+    this.theApp.grooveRequestObserver.register();
     // to be or not to be
-    if(gpreferences.prefHasUserValue("enabled")){
-    	if(gpreferences.getBoolPref("enabled")){
-    		grooveshredder.setEnabled();
+    if(this.theApp.gpreferences.prefHasUserValue("enabled")){
+    	if(this.theApp.gpreferences.getBoolPref("enabled")){
+    		this.setEnabled();
     	} else {
-    		grooveshredder.setDisabled();
+    		this.setDisabled();
     	}
     } else {
-    	grooveshredder.setEnabled();
+    	this.setEnabled();
     }
   },
   onToolbarButtonCommand: function(e) {
@@ -29,48 +31,51 @@ var grooveshredder = {
   setEnabled: function() {
   		var btn = document.getElementById("grooveshredder-toolbar-button");
 		this.initialized = true;
-		gpreferences.setBoolPref("enabled", true);
-		grooveRequestObserver.register();
+		this.theApp.gpreferences.setBoolPref("enabled", true);
+		this.theApp.grooveRequestObserver.register();
 		btn.setAttribute("class","grooveshredder-tbutton-on toolbarbutton-1 chromeclass-toolbar-additional");  
   },
   setDisabled: function() {
   		var btn = document.getElementById("grooveshredder-toolbar-button");
 		this.initialized = false;
-		gpreferences.setBoolPref("enabled", false);
-		grooveRequestObserver.unregister();
+		this.theApp.gpreferences.setBoolPref("enabled", false);
+		this.theApp.grooveRequestObserver.unregister();
 		btn.setAttribute("class","grooveshredder-tbutton-off toolbarbutton-1 chromeclass-toolbar-additional");
-  }
+  },
+  theApp : orgArgeeCodeGrooveShredder
 };
 
-var grooveRequestObserver =
+orgArgeeCodeGrooveShredder.grooveRequestObserver = 
 {
-  observe: function(subject, topic, data)
-  {
-	if(typeof(Components) !== 'undefined'){
-		var channel = subject.QueryInterface(Components.interfaces.nsIHttpChannel);
-		var re = /http:\/\/grooveshark.com\/more.php\?getStreamKeyFromSongIDEx$/;
-		if(channel.URI.spec.match(re)){
-			this.original = this.original? false : true;
-			if(this.original) createButton(getPostData(subject), 0);
+	observe: function(subject, topic, data)
+	{
+		if(typeof(Components) !== 'undefined'){
+			var channel = subject.QueryInterface(Components.interfaces.nsIHttpChannel);
+			var re = /http:\/\/grooveshark.com\/more.php\?getStreamKeyFromSongIDEx$/;
+			if(channel.URI.spec.match(re)){
+				this.original = this.original? false : true;
+				if(this.original) this.theApp.utility.createButton(this.theApp.utility.getPostData(subject), 0);
+			}
 		}
-	}
-  },
-  get observerService() {
-    return Components.classes["@mozilla.org/observer-service;1"]
-                     .getService(Components.interfaces.nsIObserverService);
-  },
-  register: function()
-  {
-	this.original = true;
-    this.observerService.addObserver(this, "http-on-modify-request", false);
-  },
-  unregister: function()
-  {
-    this.observerService.removeObserver(this, "http-on-modify-request");
-  }
+	},
+	get observerService() {
+		return Components.classes["@mozilla.org/observer-service;1"]
+						 .getService(Components.interfaces.nsIObserverService);
+	},
+	register: function()
+	{
+		this.original = true;
+		this.observerService.addObserver(this, "http-on-modify-request", false);
+	},
+	unregister: function()
+	{
+		this.observerService.removeObserver(this, "http-on-modify-request");
+	},
+	theApp : orgArgeeCodeGrooveShredder
 };
 
-var grooveDownloader = {
+orgArgeeCodeGrooveShredder.grooveDownloader = 
+{
 	init: function(url, dataString) {
 		this.ios = Components.classes['@mozilla.org/network/io-service;1']
 						.getService(Components.interfaces.nsIIOService);
@@ -79,7 +84,7 @@ var grooveDownloader = {
 		this.xfer = Components.classes["@mozilla.org/transfer;1"].createInstance(Components.interfaces.nsITransfer);
 		
 		this.url = 'http://'+url+'/stream.php';
-		this.data = newPostData(dataString);
+		this.data = this.theApp.utility.newPostData(dataString);
 	},
 	getFileName: function()
 	{
@@ -90,14 +95,14 @@ var grooveDownloader = {
 		var song_name = songDetails.match(name_regex)[1].replace("&amp;", "&");
 		var song_artist = songDetails.match(from_regex)[1].replace("&amp;", "&");
 		var song_album = songDetails.match(album_regex)[1].replace("&amp;", "&");
-		var file_pref = gpreferences.getCharPref(".filename");
+		var file_pref = this.theApp.gpreferences.getCharPref(".filename");
 		this.song_file = file_pref.replace("%artist%", song_artist)
 									.replace("%title%", song_name)
 										.replace("%album%", song_album).replace("\\", "") + ".mp3";
 	},
 	runFilePicker: function()
 	{
-		if(!gpreferences.getBoolPref(".nodialog")){
+		if(!this.theApp.gpreferences.getBoolPref(".nodialog")){
 			var nsIFilePicker = Components.interfaces.nsIFilePicker;
 			var fp = Components.classes["@mozilla.org/filepicker;1"].createInstance(nsIFilePicker);
 			fp.init(window, "Save GrooveShredded File As...", nsIFilePicker.modeSave);
@@ -113,7 +118,7 @@ var grooveDownloader = {
 			else return false;
 		} else {
 			Components.utils.import("resource://gre/modules/FileUtils.jsm");
-			this.thefile = FileUtils.getFile("ProfD", [this.song_file]);
+			this.thefile = FileUtils.getFile("Desk", [this.song_file]);
 			this.obj_URI = this.ios.newURI(this.url, null, null);
 			this.file_URI = this.ios.newFileURI(this.thefile);
 			if(this.thefile.exists()){
@@ -137,76 +142,80 @@ var grooveDownloader = {
 		if(this.runFilePicker()){
 			this.saveSong();
 		}
-	}
+	},
+	theApp : orgArgeeCodeGrooveShredder
 };
 
-function createButton(iden, times){
-	$.ajax({
-		url: 'http://grooveshark.com/more.php?getStreamKeyFromSongIDEx=',
-		type: 'POST',
-		data: iden,
-		dataType: 'text',
-		contentType: 'application/json',
-		success: function(result) {
-			// Repeat 10x to ensure fresh key
-			if(times<10) createButton(iden, times+1);
-			var element;
-			var url_patt = /"ip":"(.*)"}/;
-			var key_patt = /"streamKey":"(.*)",/;
-			var stream_url = result.match(url_patt)[1];
-			var stream_key = result.match(key_patt)[1];
-			// Add a button to grooveshark
-			element = content.document.getElementById("playerDetails_nowPlaying");
-			$(element).children('b').remove();
-			$(element).append('<b id="playerDetails_grooveShredder" style="\
-								cursor:pointer; \
-								color: #fff; \
-								font-weight: normal; \
-								margin-left: 10px; \
-								padding: 2px 4px 2px 24px; \
-								-moz-border-radius: 2px; \
-								background: #888 url(chrome://grooveshredder/skin/shark-s.png) no-repeat 4px center;"> \
-								Download Song</b>');
-			$(element).children('b').click(function(){
-				grooveDownloader.execute(stream_url, stream_key);
-			});
-			
-			// Autodownload if preferred
-			if(gpreferences.getBoolPref(".autoget") && times == 10){
-				grooveDownloader.execute(stream_url, stream_key);
+orgArgeeCodeGrooveShredder.utility = 
+{
+	createButton: function(iden, times){
+		var theApp = orgArgeeCodeGrooveShredder;
+		$.ajax({
+			url: 'http://grooveshark.com/more.php?getStreamKeyFromSongIDEx=',
+			type: 'POST',
+			data: iden,
+			dataType: 'text',
+			contentType: 'application/json',
+			success: function(result) {
+				// Repeat 10x to ensure fresh key
+				if(times<10) orgArgeeCodeGrooveShredder.utility.createButton(iden, times+1);
+				var element;
+				var url_patt = /"ip":"(.*)"}/;
+				var key_patt = /"streamKey":"(.*)",/;
+				var stream_url = result.match(url_patt)[1];
+				var stream_key = result.match(key_patt)[1];
+				// Add a button to grooveshark
+				element = content.document.getElementById("playerDetails_nowPlaying");
+				$(element).children('b').remove();
+				$(element).append('<b id="playerDetails_grooveShredder" style="\
+									cursor:pointer; \
+									color: #fff; \
+									font-weight: normal; \
+									margin-left: 10px; \
+									padding: 2px 4px 2px 24px; \
+									-moz-border-radius: 2px; \
+									background: #888 url(chrome://grooveshredder/skin/shark-s.png) no-repeat 4px center;"> \
+									Download Song</b>');
+				$(element).children('b').click(function(){
+					theApp.grooveDownloader.execute(stream_url, stream_key);
+				});
+				
+				// Autodownload if preferred
+				if(theApp.gpreferences.getBoolPref(".autoget") && times == 10){
+					theApp.grooveDownloader.execute(stream_url, stream_key);
+				}
 			}
-		}
-	});
+		});
+	},
+	getPostData: function(subject){
+		var dataStream = subject.QueryInterface(Components.interfaces.nsIUploadChannel).uploadStream;
+		dataStream.QueryInterface(Ci.nsISeekableStream).seek(Ci.nsISeekableStream.NS_SEEK_SET, 0);
+		var stream = Cc["@mozilla.org/binaryinputstream;1"].createInstance(Ci.nsIBinaryInputStream);
+		stream.setInputStream(dataStream);
+		var postBytes = stream.readByteArray(stream.available());
+		var poststr = String.fromCharCode.apply(null, postBytes);
+		dataStream.QueryInterface(Ci.nsISeekableStream).seek(Ci.nsISeekableStream.NS_SEEK_SET, 0);
+		var re = /{.*}/;
+		return poststr.match(re)[0];
+	},
+	newPostData: function(dataString){
+		dataString = 'streamKey='+dataString.replace('_', '%5F');
+		const Cc = Components.classes;
+		const Ci = Components.interfaces;
+		var stringStream = Cc["@mozilla.org/io/string-input-stream;1"].
+						   createInstance(Ci.nsIStringInputStream);
+		if ("data" in stringStream) // Gecko 1.9 or newer
+		  stringStream.data = dataString;
+		else // 1.8 or older
+		  stringStream.setData(dataString, dataString.length);
+		var postData = Cc["@mozilla.org/network/mime-input-stream;1"].
+					   createInstance(Ci.nsIMIMEInputStream);
+		postData.addHeader("Content-Type", "application/x-www-form-urlencoded");
+		postData.addContentLength = true;
+		postData.setData(stringStream);
+		return postData;
+	},
+	theApp : orgArgeeCodeGrooveShredder
 }
 
-function getPostData(subject){
-	var dataStream = subject.QueryInterface(Components.interfaces.nsIUploadChannel).uploadStream;
-	dataStream.QueryInterface(Ci.nsISeekableStream).seek(Ci.nsISeekableStream.NS_SEEK_SET, 0);
-	var stream = Cc["@mozilla.org/binaryinputstream;1"].createInstance(Ci.nsIBinaryInputStream);
-	stream.setInputStream(dataStream);
-	var postBytes = stream.readByteArray(stream.available());
-	var poststr = String.fromCharCode.apply(null, postBytes);
-	dataStream.QueryInterface(Ci.nsISeekableStream).seek(Ci.nsISeekableStream.NS_SEEK_SET, 0);
-	var re = /{.*}/;
-	return poststr.match(re)[0];
-}
-
-function newPostData(dataString){
-	dataString = 'streamKey='+dataString.replace('_', '%5F');
-	const Cc = Components.classes;
-	const Ci = Components.interfaces;
-	var stringStream = Cc["@mozilla.org/io/string-input-stream;1"].
-					   createInstance(Ci.nsIStringInputStream);
-	if ("data" in stringStream) // Gecko 1.9 or newer
-	  stringStream.data = dataString;
-	else // 1.8 or older
-	  stringStream.setData(dataString, dataString.length);
-	var postData = Cc["@mozilla.org/network/mime-input-stream;1"].
-				   createInstance(Ci.nsIMIMEInputStream);
-	postData.addHeader("Content-Type", "application/x-www-form-urlencoded");
-	postData.addContentLength = true;
-	postData.setData(stringStream);
-	return postData;
-}
-
-window.addEventListener("load", function () { grooveshredder.onLoad(); }, false);
+window.addEventListener("load", function () { orgArgeeCodeGrooveShredder.grooveshredder.onLoad(); }, false);
