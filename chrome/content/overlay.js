3121304e159a3a007bbadccefe9f6c819e07b290
rgarg8 <cryptolemy@gmail.com>
2011-07-27 11:57:36 -0400

    Reverting to No-API, No-Nonsense ways. Added better playlist downloading, need to integrate it.

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -76,13 +76,10 @@ orgArgeeCodeGrooveShredder.grooveRequestObserver =
 
 			// Perform the actions appropriate to the event
 			if(channel.URI.spec.match(token_url)){
-				alert(1);
 				// Grooveshark was opened in this tab, remember the tab
 				var notificationCallbacks = channel.notificationCallbacks;
 				var domWin = notificationCallbacks.getInterface(Components.interfaces.nsIDOMWindow);
 				this.theApp.browser = gBrowser.getBrowserForDocument(domWin.top.document);
-				// Fetch a communication token for us to use
-				this.theApp.utility.setCommToken(this.theApp.utility.getPostData(subject));
 			} else if(channel.URI.spec.match(song_url)){
 				// Simple toggle to prevent two requests for the price of one
 				this.originalSng = this.originalSng? false : true;
@@ -223,39 +220,11 @@ orgArgeeCodeGrooveShredder.grooveDownloader =
 orgArgeeCodeGrooveShredder.utility = 
 {
 	/**
-	 * Fetch a communication token and save it.
-	 **/
-	setCommToken: function(postdata){
-		$grooveShredderQuery.ajax({
-			url: 'http://grooveshark.com/more.php?getCommunicationToken=',
-			type: 'POST',
-			data: postdata,
-			dataType: 'text',
-			contentType: 'application/json',
-			success: function(result) {
-				// This expression will match the token
-				var exp = /"result":"([^"]+)"/g;
-				var res = exp.exec(result);
-				// Store the token in a safe place
-				orgArgeeCodeGrooveShredder.commToken = res[1];
-			}
-		});
-	},
-	/**
-	 * Generate a one-time request token.
-	 **/
-	getRequestToken: function(method){
-		var theApp = orgArgeeCodeGrooveShredder;
-		var sixRndChar = "aaaaaa"
-		var totalToken = method + ":" + theApp.commToken + ":quitStealinMahShit:" + sixRndChar;
-		totalToken = theApp.utility.generateSha1(totalToken);
-		return sixRndChar + totalToken;
-	},
-	/**
 	 * Create the "Download Song" button and append it to the page.
 	 **/
 	createButton: function(iden, times){
 		var theApp = orgArgeeCodeGrooveShredder;
+		theApp.streamToken = iden.match(/[0-9a-z]{46}/g);
 		$grooveShredderQuery.ajax({
 			url: 'http://grooveshark.com/more.php?getStreamKeyFromSongIDEx=',
 			type: 'POST',
@@ -315,11 +284,20 @@ orgArgeeCodeGrooveShredder.utility =
 																Download Song</b>');
 				$grooveShredderQuery(element).children('b').click(function(){
 					songIds.forEach(function(songId){
+							// Strip out the numerical song ID
+							songId = songId.match(/[0-9]+/);
 							// Prepare the POST data
-							var toSend = '{"method":"getStreamKeyFromSongIDEx","parameters":{"mobile":false,"prefetch":false,'+ country +','+ songId +'},'+ headers +'}';
+							var toSend = '{"method":"getStreamKeyFromSongIDEx",\
+										   "parameters":\
+												{"mobile":false,\
+												 "prefetch":false,\
+												 "songID":'+ songId +',\
+												 '+ country +'},\
+										  '+ headers +'}';
 							// Replace invalid client
 							toSend = toSend.replace("htmlshark", "jsqueue");
-							alert(toSend);
+							// Replace invalid token
+							toSend = toSend.replace(/[0-9a-z]{46}/g,theApp.streamToken);
 							// Fetch the stream key
 							$grooveShredderQuery.ajax({
 								url: 'http://grooveshark.com/more.php?getStreamKeyFromSongIDEx=',
@@ -441,163 +419,6 @@ orgArgeeCodeGrooveShredder.utility =
 		}
 		
 		return directory;
-	},
-	/**
-	 * Generate a SHA-1 hash given a plaintext string.
-	 */
-	generateSha1: function(msg){
-		function rotate_left(n,s) {
-			var t4 = ( n<<s ) | (n>>>(32-s));
-			return t4;
-		};
-
-		function lsb_hex(val) {
-			var str="";
-			var i;
-			var vh;
-			var vl;
-			for( i=0; i<=6; i+=2 ) {
-				vh = (val>>>(i*4+4))&0x0f;
-				vl = (val>>>(i*4))&0x0f;
-				str += vh.toString(16) + vl.toString(16);
-			}
-			return str;
-		};
-
-		function cvt_hex(val) {
-			var str="";
-			var i;
-			var v;
-			for( i=7; i>=0; i-- ) {
-				v = (val>>>(i*4))&0x0f;
-				str += v.toString(16);
-			}
-			return str;
-		};
-
-		function Utf8Encode(string) {
-			string = string.replace(/\r\n/g,"\n");
-			var utftext = "";
-
-			for (var n = 0; n < string.length; n++) {
-				var c = string.charCodeAt(n);
-				if (c < 128) {
-					utftext += String.fromCharCode(c);
-				}
-				else if((c > 127) && (c < 2048)) {
-					utftext += String.fromCharCode((c >> 6) | 192);
-					utftext += String.fromCharCode((c & 63) | 128);
-				}
-				else {
-					utftext += String.fromCharCode((c >> 12) | 224);
-					utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-					utftext += String.fromCharCode((c & 63) | 128);
-				}
-			}
-			return utftext;
-		};
-
-		var blockstart;
-		var i, j;
-		var W = new Array(80);
-		var H0 = 0x67452301;
-		var H1 = 0xEFCDAB89;
-		var H2 = 0x98BADCFE;
-		var H3 = 0x10325476;
-		var H4 = 0xC3D2E1F0;
-		var A, B, C, D, E;
-		var temp;
-
-		msg = Utf8Encode(msg);
-
-		var msg_len = msg.length;
-
-		var word_array = new Array();
-		for( i=0; i<msg_len-3; i+=4 ) {
-			j = msg.charCodeAt(i)<<24 | msg.charCodeAt(i+1)<<16 |
-			msg.charCodeAt(i+2)<<8 | msg.charCodeAt(i+3);
-			word_array.push( j );
-		}
-
-		switch( msg_len % 4 ) {
-			case 0:
-				i = 0x080000000;
-			break;
-			case 1:
-				i = msg.charCodeAt(msg_len-1)<<24 | 0x0800000;
-			break;
-
-			case 2:
-				i = msg.charCodeAt(msg_len-2)<<24 | msg.charCodeAt(msg_len-1)<<16 | 0x08000;
-			break;
-
-			case 3:
-				i = msg.charCodeAt(msg_len-3)<<24 | msg.charCodeAt(msg_len-2)<<16 | msg.charCodeAt(msg_len-1)<<8	| 0x80;
-			break;
-		}
-
-		word_array.push( i );
-
-		while( (word_array.length % 16) != 14 ) word_array.push( 0 );
-
-		word_array.push( msg_len>>>29 );
-		word_array.push( (msg_len<<3)&0x0ffffffff );
-
-		for ( blockstart=0; blockstart<word_array.length; blockstart+=16 ) {
-			for( i=0; i<16; i++ ) W[i] = word_array[blockstart+i];
-			for( i=16; i<=79; i++ ) W[i] = rotate_left(W[i-3] ^ W[i-8] ^ W[i-14] ^ W[i-16], 1);
-
-			A = H0;
-			B = H1;
-			C = H2;
-			D = H3;
-			E = H4;
-
-			for( i= 0; i<=19; i++ ) {
-				temp = (rotate_left(A,5) + ((B&C) | (~B&D)) + E + W[i] + 0x5A827999) & 0x0ffffffff;
-				E = D;
-				D = C;
-				C = rotate_left(B,30);
-				B = A;
-				A = temp;
-			}
-
-			for( i=20; i<=39; i++ ) {
-				temp = (rotate_left(A,5) + (B ^ C ^ D) + E + W[i] + 0x6ED9EBA1) & 0x0ffffffff;
-				E = D;
-				D = C;
-				C = rotate_left(B,30);
-				B = A;
-				A = temp;
-			}
-
-			for( i=40; i<=59; i++ ) {
-				temp = (rotate_left(A,5) + ((B&C) | (B&D) | (C&D)) + E + W[i] + 0x8F1BBCDC) & 0x0ffffffff;
-				E = D;
-				D = C;
-				C = rotate_left(B,30);
-				B = A;
-				A = temp;
-			}
-
-			for( i=60; i<=79; i++ ) {
-				temp = (rotate_left(A,5) + (B ^ C ^ D) + E + W[i] + 0xCA62C1D6) & 0x0ffffffff;
-				E = D;
-				D = C;
-				C = rotate_left(B,30);
-				B = A;
-				A = temp;
-			}
-
-			H0 = (H0 + A) & 0x0ffffffff;
-			H1 = (H1 + B) & 0x0ffffffff;
-			H2 = (H2 + C) & 0x0ffffffff;
-			H3 = (H3 + D) & 0x0ffffffff;
-			H4 = (H4 + E) & 0x0ffffffff;
-		}
-
-		var temp = cvt_hex(H0) + cvt_hex(H1) + cvt_hex(H2) + cvt_hex(H3) + cvt_hex(H4);
-		return temp.toLowerCase();
 	},
 	theApp : orgArgeeCodeGrooveShredder
 }
