b7d605d1d3a4cbf361b8996fab16e1178f6dc892
rgarg8 <cryptolemy@gmail.com>
2011-08-11 22:22:43 -0400

    Switched to using AMO API rather than scraping.

--- chrome/content/options.js
+++ chrome/content/options.js
@@ -45,9 +45,8 @@ $grooveShredderQuery(function(){
 		}
 	});
 	
-	$grooveShredderQuery.get('https://addons.mozilla.org/en-US/firefox/addon/grooveshredder/', function(data){
-		var re = /<span class="version-number">([0-9.]+)<\/span>/i
-		var version = re.exec(data)[1];
+	$grooveShredderQuery.get('https://services.addons.mozilla.org/en-US/firefox/api/1.3/addon/305091', function(data){
+		var version = data.getElementsByTagName("version")[0].textContent;
 		var my_version = '1.14.5';
 		if(version > my_version){
 			$grooveShredderQuery('#abovecontainer .orange').css('display', 'block');
